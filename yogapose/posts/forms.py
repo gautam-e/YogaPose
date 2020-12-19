9e082958f322d64389503da070cb6e207d183bf9
Gautam <gautam.e@gmail.com>
2020-12-19 14:00:27 +0100

    Removed PNGs

--- yogapose/posts/forms.py
+++ yogapose/posts/forms.py
@@ -4,7 +4,6 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired
 
 class PostPoseForm(FlaskForm):
-    pose_pic = FileField(label='Select an image', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!'), DataRequired()])
     submit = SubmitField('Upload and score')
 
 class UpdatePostPoseForm(FlaskForm):
