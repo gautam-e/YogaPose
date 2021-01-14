d8f1b1ff8b00922fd846572b4f39e43a4cd5a227
Gautam <gautam.e@gmail.com>
2021-01-14 13:53:07 +0100

    capture user and accept image/*

--- yogapose/posts/forms.py
+++ yogapose/posts/forms.py
@@ -4,7 +4,8 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired
 
 class PostPoseForm(FlaskForm):
-    pose_pic = FileField(label='Select an image', validators=[FileAllowed(['jpg', 'jpeg'], 'JPGs only!'), DataRequired()])
+    render_kw={'capture':"user", 'accept':"image/*"}
+    pose_pic = FileField(label='Select an image', validators=[FileAllowed(['jpg', 'jpeg'], 'JPGs only!'), DataRequired()], render_kw=render_kw)
     submit = SubmitField('Upload and score')
 
 class UpdatePostPoseForm(FlaskForm):
